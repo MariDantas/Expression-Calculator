71da72084fde9a9707b484fd25dbe2d65b00697c
MariDantas <61691909+MariDantas@users.noreply.github.com>
2022-04-26 22:37:56 -0300

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -9,6 +9,12 @@ public class Calculator {
 		double newSum = 0;
 		DecimalFormat decimalFormat = new DecimalFormat("#.####");
 		Scanner read = new Scanner(System.in);
+		double currSum;
+		String formattedExp;
+		long roundJ;
+		long roundI;
+		String formattedNewSum = "";
+		String formattedCurrSum;
 		
 		try {
 			System.out.print("Enter a number greater than or equal to 2: ");
@@ -18,25 +24,25 @@ public class Calculator {
 				double j = number;
 				
 				for (double i = 1; i < number; i++) {
-					double currSum = newSum;
-					String formattedExp = decimalFormat.format((j * (j-1))/i);
-					long roundJ = Math.round(j);
-					long roundI = Math.round(i);
+					currSum = newSum;
+					formattedExp = decimalFormat.format((j * (j-1))/i);
+					roundJ = Math.round(j);
+					roundI = Math.round(i);
 					
 					TimeUnit.SECONDS.sleep(2);
 					System.out.println("\n(" + roundJ + " * " + (roundJ-1) + ")/" + roundI + " = " + formattedExp);
 					if (i == 1 || i % 2 == 0) {
 						
 						newSum += (j * (j-1))/i;
-						String formattedNewSum = decimalFormat.format(newSum);
-						String formattedCurrSum = decimalFormat.format(currSum);
+						formattedNewSum = decimalFormat.format(newSum);
+						formattedCurrSum = decimalFormat.format(currSum);
 						
 						System.out.println(formattedCurrSum + " + " + formattedExp + " = " + formattedNewSum);
 					} else {
 						
 						newSum -= (j * (j-1))/i;
-						String formattedNewSum = decimalFormat.format(newSum);
-						String formattedCurrSum = decimalFormat.format(currSum);
+						formattedNewSum = decimalFormat.format(newSum);
+						formattedCurrSum = decimalFormat.format(currSum);
 						
 						System.out.println(formattedCurrSum + " - " + formattedExp + " = " + formattedNewSum);
 					}
@@ -44,7 +50,7 @@ public class Calculator {
 					j--;
 				}
 				
-				System.out.println("\n\nThe result is: " + decimalFormat.format(newSum));
+				System.out.println("\n\nThe result is: " + formattedNewSum);
 				
 			} else {
 				System.out.println("Invalid number!");
